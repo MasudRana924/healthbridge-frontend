5194e9f1914d52e4feaf4485168e1318691f51ea
MasudBS-1458 <masud.rana@brainstation-23.com>
2026-01-16 18:31:50 +0600

    doctor api updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css'
 import AppRoutes from './routes/AppRoutes';
+import ScrollToTop from './components/common/ScrollToTop';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="flex flex-col min-h-screen">
         <main className="flex-grow">
           <AppRoutes />
